91c7c91955791333e9c439a231415ad8494a37bc
Laith Harb <laithharb@Laiths-iMac.local>
2022-10-04 11:31:26 -0400

    add nodemon

--- starter/notifications-api/index.js
+++ starter/notifications-api/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("NOTIFCATIONS API")
+})
+
 app.listen(8001, () => {
   console.log("Listening on PORT 8001");
 });
